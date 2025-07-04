8ef56afe03f69d1ab9b10c433710fa8306d71744
matthewKeville <matthewkeville@protonmail.com>
2025-07-04 15:44:34 -0400

    lombok configuration for bandcamp-scraper-models

--- bandcamp-scraper-models/src/main/java/bandcamp_scraper_models/Artist.java
+++ bandcamp-scraper-models/src/main/java/bandcamp_scraper_models/Artist.java
@@ -2,16 +2,25 @@ package bandcamp_scraper_models;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Artist {
 
   private String name;
   private String location;
   private List<String> releases;
-
-  public Artist(String name, String location, List<String> releases) {
-    this.name = name;
-    this.location = location;
-    this.releases = releases;
-  }
 
 }
